798de19b232ad13542861adf32cd5961aa412801
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-02-28 20:00:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,7 @@
 def square_array(numbers)
-  squared_numbers = numbers.each do
+  squared_numbers = []
+  numbers.each do |number|
+    squared_numbers.push(number * number)
   end
   squared_numbers
 end
