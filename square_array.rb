5d69cf96c27bfe0834d68261e2ee36745e5be8a3
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-02-28 16:37:38 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(numbers)
   numbers = [1, 2, 3]
-  numbers_squared = numbers.each { |number|  ** 2}
+  numbers_squared = numbers.each { |number| #{number} ** 2}
   numbers_squared.push
   end
 end
